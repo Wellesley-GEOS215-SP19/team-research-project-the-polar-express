23bcd61ca30b43d3d32d043ca287f70c96d8f3d6
Jharri11 <47122728+Jharri11@users.noreply.github.com>
2019-04-03 10:01:56 -0400

    figure 1

--- poles_model.m
+++ poles_model.m
@@ -16,10 +16,32 @@ timeyear= time./365
 %%
 marchs = NaN([size(lat, 1) size(lon, 1) 93]);
 vector= 3:12:1140
-
 marchs= model(:,:,vector);
      
-%%% figure
+%% figure from 2006
+load coastlines
+figure('Color','w'); clf
+axesm('eqaazim','MapLatLimit',[50 90])
+axis off
+framem on
+gridm on
+mlabel on
+plabel on;
+setm(gca,'MLabelParallel',0)
+geoshow(coastlat,coastlon,'DisplayType','polygon', 'DefaultFaceColor','[0, 0.5, 0]','DefaultEdgeColor','black')
+contourfm(lat, lon, model(:,:,1)','linecolor','none');
+colormap(cmocean('ice')); colorbar;
+title ('2006 january sea ice concentration')
+
+%%
+
+figure(1); clf
+worldmap world
+contourfm(model(:,:,1),'linecolor','none');
+colorbar
+geoshow('landareas.shp','FaceColor','black')
+title('January Sea Surface Temperature (^oC)')
+
 
 
 
