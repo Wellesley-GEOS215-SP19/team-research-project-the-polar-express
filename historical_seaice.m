445b479af73ce3a8c0905c006f386053757d0907
Anna Gaskill <agaskill@wellesley.edu>
2019-04-03 13:48:46 -0400

    4/3 historical sea ice (anna)
    attempt to read in files on sea ice thickness, not successful

--- historical_seaice.m
+++ historical_seaice.m
@@ -2,5 +2,25 @@
 
 %% uploading data
 
+seaice = netcdf.open('sit_OImon_ensemble_historical_190001-200512.nc', 'NOWRITE');
+seaice2 = netcdf.open('sit_OImon_CCSM4_historical_r1i1p1_190001-200512.nc', 'NOWRITE');
 
+ncid = 'sit_OImon_ensemble_historical_190001-200512.nc';
+ncid1 = 'sit_OImon_CCSM4_historical_r1i1p1_190001-200512.nc';
+
+ncinfo('seaice');
+
+ncdisp('ncid');
+
+%reading in variables
+lon = ncread('seaice', 'lon');
+whos seaice
+lat = ncread('seaice', 'lat');
+whos seaice
+time = ncread('seaice', 'time');
+whos seaice
+time_bnds = ncread('seaice', 'time_bnds');
+whos seaice
+sit = ncread('seaice', 'sit');
+whos seaice
 
