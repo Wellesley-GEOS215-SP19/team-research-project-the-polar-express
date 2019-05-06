21714ed000872f3b13d2797813a2c6c59d132b6a
sparjan <sparjan@wellesley.edu>
2019-05-06 12:34:12 -0400

    added y=0 line

--- rate_of_change.m
+++ rate_of_change.m
@@ -15,14 +15,15 @@ for i = 1:size(a)
 end
 
 for i = 1:size(b)
-    d(i) = 1
+    d(i) = 0
 end
 
 plot(c,b)
 hold on;
 plot(c,a)
 hold on;
-plot(c,d)
-title('Change in Sea Ice Concentration Under RCP 2.6 & 8.5')
-legend('RCP 2.6','RCP 8.5','Baseline (2006)')
+P = plot(c,d)
+set(P,'Color','black')
+title('Change in Sea Ice Concentration Under RCP 2.6 & 8.5 (Relative to 2006 Baseline)')
+legend('RCP 2.6','RCP 8.5')
         
